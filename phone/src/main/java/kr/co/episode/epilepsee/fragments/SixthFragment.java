921c5ee81bebdcdf28dbcc7598e288d4dbb6654c
DESKTOP-T05A3CM\YINA <dlqueen16@sookmyung.ac.kr>
2023-08-14 23:19:35 +0900

    SixthFragment까지 ViewModel 만들기 완료 23.08.14.23:19

--- phone/src/main/java/kr/co/episode/epilepsee/fragments/SixthFragment.java
+++ phone/src/main/java/kr/co/episode/epilepsee/fragments/SixthFragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -16,6 +19,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -39,7 +44,12 @@ public class SixthFragment extends Fragment {
     private Button placeButton3;
     private Button placeButton4;
     private Button placeButton5;
+    private CheckBox sleepCheckbox;
 
+    private EditText emergencyMedicationEditText;
+    private Button addButton;
+    private TextView emergencyMedicationListTextView;
+    private List<String> addedMedications = new ArrayList<>(); // 약물 이름 저장하는 리스트
     private SeizureViewModel seizureViewModel;
 
     @Nullable
@@ -57,8 +67,7 @@ public class SixthFragment extends Fragment {
         placeButton4 = rootView.findViewById(R.id.placeButton4);
         placeButton5 = rootView.findViewById(R.id.placeButton5);
 
-        //발생장소 버튼에 데이터저장 연결
-
+        //발생장소 버튼에 이벤트 처리
         placeButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,14 +102,16 @@ public class SixthFragment extends Fragment {
             }
         });
 
-        //발작 지속 시간선택
-        Button timeSet = rootView.findViewById(R.id.timeSet);
-        timeSet.setOnClickListener(new View.OnClickListener() {
+        // 수면 중 여부 체크박스 초기화
+        sleepCheckbox = rootView.findViewById(R.id.sleepCheckbox);
+        // 수면 중 여부 체크박스 클릭 이벤트 처리
+        sleepCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                showTimePickerDialog();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                seizureViewModel.setSeizureDuringSleep(isChecked);
             }
         });
+
         //회복에 걸린 시간 선택
         recoveryTimeSpinner = rootView.findViewById(R.id.recoveryTimeSpinner);
         recoveryTimeTextView =rootView.findViewById(R.id.recoveryTimeTextView);
@@ -117,6 +128,8 @@ public class SixthFragment extends Fragment {
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
                 String recoveryTime = recoveryTimes[position];
                 recoveryTimeTextView.setText(recoveryTime);
+                //ViewModel에 저장
+                seizureViewModel.setRecoveryTime(recoveryTime);
             }
 
             @Override
@@ -124,9 +137,39 @@ public class SixthFragment extends Fragment {
                 //아무것도 선택하지 않았을 때
                 String defaultRecoveryTime = "5분";
                 recoveryTimeTextView.setText(defaultRecoveryTime);
+                //ViewModel에 저장
+                seizureViewModel.setRecoveryTime(defaultRecoveryTime);
+            }
+        });
+
+        //발작 지속 시간선택
+        Button timeSet = rootView.findViewById(R.id.timeSet);
+        timeSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePickerDialog();
+            }
+        });
+
+        //EditText, addButton, TextView 초기화 (사용된 긴급약물)
+        emergencyMedicationEditText = rootView.findViewById(R.id.emergencyMedicationEditText);
+        addButton = rootView.findViewById(R.id.addButton);
+        emergencyMedicationListTextView = rootView.findViewById(R.id.emergencyMedicationListTextView);
+
+        // 추가 버튼 클릭 이벤트 처리
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String medicine = emergencyMedicationEditText.getText().toString();
+                if(!medicine.isEmpty()){
+                    addedMedications.add(medicine);
+                    updateEmergencyMedicationListTextView();
+                    emergencyMedicationEditText.setText("");
+                }
             }
         });
         return rootView;
+
     }
 
 
@@ -143,7 +186,15 @@ public class SixthFragment extends Fragment {
                         selectedMinute = minute;
                         selectedSecond = 0; // Reset the selected second to 0
 
-                        // TODO: Optionally, you can update the UI to reflect the selected time
+                        //선택한 총 지속 시간 계산
+                        int totalDuration = (hourOfDay * 60) + minute;
+
+                        String formattedDuration = String.format("%02d:%02d", hourOfDay, minute);
+                        // UI 업데이트하여 선택한 지속 시간 표시
+                        recoveryTimeTextView.setText(formattedDuration);
+
+                        // 선택한 지속 시간을 SeizureViewModel에 저장
+                        seizureViewModel.setSeizureDuration(formattedDuration);
                     }
                 },
                 hour,
@@ -152,5 +203,17 @@ public class SixthFragment extends Fragment {
         );
 
         timePickerDialog.show();
+
+    }
+    // emergencyMedicationListTextView 업데이트
+    private void updateEmergencyMedicationListTextView(){
+        StringBuilder medicationsText = new StringBuilder();
+        for (String medication : addedMedications){
+            medicationsText.append("- ").append(medication).append("\n");
+
+        }
+        emergencyMedicationListTextView.setText(medicationsText.toString());
+
+        seizureViewModel.setEmergencyMedication(medicationsText.toString());
     }
 }
