c2a8f07320dc8bd1a81af9c4eedccb870ffba527
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-08-14 20:32:42 +0900

    seizureStart -> seizureTime 으로 변경 후 잘 돌아가는거 확인

--- wear/src/main/java/kr/co/episode/epilepsee/SeizureCompleteActivity.java
+++ wear/src/main/java/kr/co/episode/epilepsee/SeizureCompleteActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import kr.co.episode.epilepsee.databinding.ActivitySeizureCompleteBinding;
 
 public class SeizureCompleteActivity extends Activity {
@@ -16,13 +19,17 @@ public class SeizureCompleteActivity extends Activity {
         binding = ActivitySeizureCompleteBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+
         // Get seizure start and duration from intent
         Intent intent = getIntent();
-        String seizureStart = intent.getStringExtra("seizureStart");
+        String seizureTime = intent.getStringExtra("seizureTime");
         String seizureDuration = intent.getStringExtra("seizureDuration");
 
+
+
         // Display seizure start and duration
-        binding.seizureStartTextview.setText(seizureStart);
+        binding.seizureStartTextview.setText(seizureTime);
         binding.seizureDurationTextview.setText(seizureDuration);
 
     }
