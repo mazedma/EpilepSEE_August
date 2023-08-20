1ad109907037f6160696d3a22fa957087362b191
HyewonChoi <hyewonwk2846@sookmyung.ac.kr>
2023-08-20 15:40:28 +0900

    [phone]MainActivity 오류 수정

--- phone/src/main/java/kr/co/episode/epilepsee/activities/MedicationScheduleActivity.java
+++ phone/src/main/java/kr/co/episode/epilepsee/activities/MedicationScheduleActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import kr.co.episode.epilepsee.databinding.ActivityMedicationScheduleBinding;
 
 public class MedicationScheduleActivity extends AppCompatActivity {
 
     ActivityMedicationScheduleBinding activityMedicationScheduleBinding;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,35 @@ public class MedicationScheduleActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("복용 일정 등록"); // 화면 제목 설정
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼
 
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("medications");
+
+        EditText editTextMedication = activityMedicationScheduleBinding.editTextMedication;
+        EditText editTextStartDate = activityMedicationScheduleBinding.editTextStartDate;
+        EditText editTextEndDate = activityMedicationScheduleBinding.editTextEndDate;
+        Button buttonSave = activityMedicationScheduleBinding.buttonSave;
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMedication();
+            }
+        });
     }
+
+    private void saveMedication() {
+        String medicationName = activityMedicationScheduleBinding.editTextMedication.getText().toString();
+        String startDate = activityMedicationScheduleBinding.editTextStartDate.getText().toString();
+        String endDate = activityMedicationScheduleBinding.editTextEndDate.getText().toString();
+
+        // Medication 객체 생성
+        Medication medication = new Medication(medicationName, startDate, endDate);
+
+        // Firebase에 데이터 저장
+        databaseReference.push().setValue(medication);
+    }
+
+
+
     // 액션바의 백 버튼 클릭 이벤트 처리
     @Override
     public boolean onSupportNavigateUp () {
